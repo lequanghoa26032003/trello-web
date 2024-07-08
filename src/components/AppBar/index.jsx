1f4b6bf3036748e3927defeaa692a7565909364f
lequanghoa26032003 <117667583+lequanghoa26032003@users.noreply.github.com>
2024-07-08 17:39:56 +0700

    trello_app_bar

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,15 +1,30 @@
 import Box from '@mui/material/Box'
-import ModeSelect from '../../components/ModeSelect'
+import ModeSelect from '~/components/ModeSelect'
+import AppsIcon from '@mui/icons-material/Apps'
+import { ReactComponent as StarIcon } from '~/assets/trello.svg'
+import SvgIcon from '@mui/material/SvgIcon'
+import Typography from '@mui/material/Typography'
 function AppBar() {
   return (
-    <Box sx={{
-      backgroundColor: 'primary.light',
+    <Box px={2} my={0.5} sx={{
       width: '100%',
       height: (theme) => theme.trelloCustom.appBarHeight,
       display: 'flex',
-      alignItems: 'center'
+      alignItems: 'center',
+      justifyContent: 'space-between'
     }}>
-      <ModeSelect/>
+      <Box sx= {{ display: 'flex', alignItems: 'center', gap:2 }} >
+        <AppsIcon sx= {{ color: 'primary.main' } } />
+        <Box sx= {{ display: 'flex', alignItems: 'center', gap:1 }}>
+          <SvgIcon component={StarIcon} inheritViewBox sx= { { color: 'primary.main' } } />
+          <Typography variant="span" sx= {{ fontSize: '1.2rem', fontWeight: 'bold', color: 'primary.main' }} >
+            Trello
+          </Typography>
+        </Box>
+      </Box>
+      <Box>
+        <ModeSelect/>
+      </Box>
     </Box>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from '~/App.jsx'
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
-import theme from './theme'
+import theme from '~/theme'
 import CssBaseline from '@mui/material/CssBaseline'
 // import GlobalStyles from '@mui/material/GlobalStyles'
 ReactDOM.createRoot(document.getElementById('root')).render(
