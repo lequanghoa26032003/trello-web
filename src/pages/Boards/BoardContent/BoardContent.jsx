7dbabbbd5c5ad93e61a21a074315fb0e134165f1
lequanghoa26032003 <117667583+lequanghoa26032003@users.noreply.github.com>
2024-08-26 16:46:46 +0700

    bai35

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -13,7 +13,8 @@ import {
   useSensor,
   useSensors,
   DragOverlay,
-  defaultDropAnimationSideEffects
+  defaultDropAnimationSideEffects,
+  closestCorners
 } from '@dnd-kit/core'
 import { useEffect, useState } from 'react'
 import { arrayMove } from '@dnd-kit/sortable'
@@ -27,21 +28,63 @@ function BoardContent( props ) {
   const touchSensor = useSensor(TouchSensor, { activationConstraint: { delay: 250, tolerance: 200 } })
   const sensors = useSensors(mouseSensor, touchSensor)
   const { board } = props
-  const [orderredColumns, setOrderredColumns] = useState([])
+  const [orderredColumns, setOrderedColumns] = useState([])
   const [activeDragItemId, setactiveDragItemId] = useState(null)
   const [activeDragItemType, setactiveDragItemType] = useState(null)
   const [activeDragItemData, setactiveDragItemData] = useState(null)
+  const [oldColumnWhenDraggingCard, setoldColumnWhenDraggingCard] = useState(null)
+
   useEffect(() => {
-    setOrderredColumns(mapOrder(board?.columns, board?.columnOrderIds, '_id'))
+    setOrderedColumns(mapOrder(board?.columns, board?.columnOrderIds, '_id'))
   }, [board])
   const findColumnByCardId = (cardId) => {
     return orderredColumns.find( column => column?.cards?.map(card => card._id).includes(cardId) )
+  }
+  const moveCardBetweenDifferentColumns = (
+    overColumn,
+    overCardId,
+    over,
+    active,
+    activeColumn,
+    activeDraggingCardId,
+    activeDraggingCardData
+  ) => {
+    setOrderedColumns(prevColumns => {
+      const overCardIndex = overColumn?.cards?.findIndex( card => card._id === overCardId)
+      let newCardIndex
+      const isBelowOverItem = active.rect.current.translated && active.rect.current.translated.top > over.rect.top + over.rect.height
+      const modifier = isBelowOverItem ? 1 : 0
+      newCardIndex = overCardIndex >= 0 ? overCardIndex + modifier : overColumn?.cards?.length + 1
+      const nextColumns = cloneDeep(prevColumns)
+      const nextActiveColumn = nextColumns.find( column => column._id === activeColumn._id )
+      const nextOverColumn = nextColumns.find( column => column._id === overColumn._id )
+      if (nextActiveColumn) {
+        nextActiveColumn.cards =nextActiveColumn.cards.filter(card => card._id !== activeDraggingCardId)
+
+        nextActiveColumn.cardOrderIds = nextActiveColumn.cards.map(card => card._id)
+      }
+      if (nextOverColumn) {
+        nextOverColumn.cards =nextOverColumn.cards.filter(card => card._id !== activeDraggingCardId)
+        nextOverColumn.cards =nextOverColumn.cards.toSpliced(
+          newCardIndex,
+          0,
+          { ...activeDraggingCardData,
+            columnId: nextOverColumn._id }
+        )
+        nextOverColumn.cardOrderIds = nextOverColumn.cards.map(card => card._id)
+
+      }
+      return nextColumns
+    })
   }
   const handleDragStart = (event) => {
     // console.log('handleDragStart', event)
     setactiveDragItemId(event?.active?.id)
     setactiveDragItemType(event?.active?.data?.current?.columnId ? ACTIVE_DRAG_ITEM_TYPE.CARD : ACTIVE_DRAG_ITEM_TYPE.COLUMN)
     setactiveDragItemData(event?.active?.data?.current)
+    if (event?.active?.data?.current?.columnId) {
+      setoldColumnWhenDraggingCard(findColumnByCardId(event?.active?.id))
+    }
   }
   const handleDragOver = (event) => {
     // console.log('handleDragOver', event)
@@ -54,29 +97,15 @@ function BoardContent( props ) {
     const overColumn = findColumnByCardId(overCardId)
     if (!activeColumn || !overColumn) return
     if (activeColumn._id !== overColumn._id) {
-      setOrderredColumns(prevColumns => {
-        const overCardIndex = overColumn?.cards?.findIndex( card => card._id === overCardId)
-        let newCardIndex
-        const isBelowOverItem = active.rect.current.translated && active.rect.current.translated.top > over.rect.top + over.rect.height
-        const modifier = isBelowOverItem ? 1 : 0
-        newCardIndex = overCardIndex >= 0 ? overCardIndex + modifier : overColumn?.cards?.length + 1
-        const nextColumns = cloneDeep(prevColumns)
-        const nextActiveColumn = nextColumns.find( column => column._id === activeColumn._id )
-        const nextOverColumn = nextColumns.find( column => column._id === overColumn._id )
-        if (nextActiveColumn) {
-          nextActiveColumn.cards =nextActiveColumn.cards.filter(card => card._id !== activeDraggingCardId)
-
-          nextActiveColumn.cardOderIds = nextActiveColumn.cards.map(card => card._id)
-        }
-        if (nextOverColumn) {
-          nextOverColumn.cards =nextOverColumn.cards.filter(card => card._id !== activeDraggingCardId)
-
-          nextOverColumn.cards =nextOverColumn.cards.toSpliced(newCardIndex, 0, activeDraggingCardData)
-          nextOverColumn.cardOderIds = nextOverColumn.cards.map(card => card._id)
-
-        }
-        return nextColumns
-      })
+      moveCardBetweenDifferentColumns (
+        overColumn,
+        overCardId,
+        over,
+        active,
+        activeColumn,
+        activeDraggingCardId,
+        activeDraggingCardData
+      )
     }
 
 
@@ -85,18 +114,54 @@ function BoardContent( props ) {
     // console.log('handleDragEnd', event)
     const { active, over } = event
     if (!over) return
-    if (active.id !== over.id) {
+    if (activeDragItemType === ACTIVE_DRAG_ITEM_TYPE.CARD && active.id !== over.id) {
+      const { id: activeDraggingCardId, data: { current: activeDraggingCardData } } = active
+      const { id: overCardId } = over
+      const activeColumn = findColumnByCardId(activeDraggingCardId)
+      const overColumn = findColumnByCardId(overCardId)
+
+      if (!activeColumn || !overColumn) return
+
+      if (oldColumnWhenDraggingCard._id !== overColumn._id) {
+        moveCardBetweenDifferentColumns (
+          overColumn,
+          overCardId,
+          over,
+          active,
+          activeColumn,
+          activeDraggingCardId,
+          activeDraggingCardData
+        )
+      } else {
+        const oldCardIndex = oldColumnWhenDraggingCard?.cards?.findIndex( c => c._id === activeDragItemId)
+        const newCardIndex = overColumn?.cards?.findIndex( c => c._id === overCardId)
+        const dndOrderedCards = arrayMove(oldColumnWhenDraggingCard?.cards, oldCardIndex, newCardIndex)
+        setOrderedColumns(prevColumns => {
+
+          const nextColumns = cloneDeep(prevColumns)
+
+          const targetColumn = nextColumns.find( column => column._id === overColumn._id )
+          targetColumn.cards = dndOrderedCards
+          targetColumn.cardOrderIds = dndOrderedCards.map(card => card._id)
+          return nextColumns
+        })
+
+      }
+
+    }
+    if (activeDragItemType===ACTIVE_DRAG_ITEM_TYPE.COLUMN && active.id !== over.id) {
       const oldIndex = orderredColumns.findIndex( c => c._id === active.id)
       const newIndex = orderredColumns.findIndex( c => c._id === over.id)
       const dndOrderredColumns = arrayMove(orderredColumns, oldIndex, newIndex)
       // const dndOrderredColumnsIds = dndOrderredColumns.map(c => c._id)
       // console.log('dndOrderredColumns', dndOrderredColumns)
       // console.log('dndOrderredColumnsIds', dndOrderredColumnsIds)
-      setOrderredColumns(dndOrderredColumns)
+      setOrderedColumns(dndOrderredColumns)
     }
     setactiveDragItemId(null)
     setactiveDragItemType(null)
     setactiveDragItemData(null)
+    setoldColumnWhenDraggingCard(null)
   }
 
   const customdropAnimation = {
@@ -109,7 +174,7 @@ function BoardContent( props ) {
     })
   }
   return (
-    <DndContext onDragStart={ handleDragStart } onDragOver={ handleDragOver } onDragEnd={ handleDragEnd } sensors={sensors} >
+    <DndContext onDragStart={ handleDragStart } onDragOver={ handleDragOver } onDragEnd={ handleDragEnd } sensors={sensors} collisionDetection={closestCorners} >
       <Box sx={{
         bgcolor: ( theme ) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2' ),
         width: '100%',
