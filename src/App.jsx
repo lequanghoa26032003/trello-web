44a0db423309ffda7835cc0bda04d3cff8d9775e
lequanghoa26032003 <117667583+lequanghoa26032003@users.noreply.github.com>
2024-11-22 05:44:33 +0700

    lesson_08

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,32 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route, Navigate, Outlet } from 'react-router-dom'
 import Board from '~/pages/Boards/_id'
 // import Boards from '~/pages/Boards'
 
 import NotFound from '~/pages/404/NotFound'
 import Auth from '~/pages/Auth/Auth'
 import AccountVerification from '~/pages/Auth/AccountVerification'
+import { useSelector } from 'react-redux'
+import { selectCurrentUser } from '~/redux/user/userSlice'
+
+const ProtectedRoute = ({ user }) => {
+  if (!user) {
+    return <Navigate to='/login' replace={true} />
+  }
+  return <Outlet />
+}
 
 function App() {
+  const currentUser = useSelector(selectCurrentUser)
+
   return (
     <Routes>
       <Route path='/' element={
         <Navigate to='/boards/6716070c2349351537af7b6b' replace={true} />
       } />
-
-      <Route path='/boards/:boardId' element={<Board/>} />
-      {/* <Route path='/boards' element={<Boards/>} /> */}
-
+      <Route element={< ProtectedRoute user={currentUser} />}>
+        <Route path='/boards/:boardId' element={<Board/>} />
+        {/* <Route path='/boards' element={<Boards/>} /> */}
+      </Route>
       <Route path='/login' element={<Auth/>} />
       <Route path='/register' element={<Auth/>} />
       <Route path='/account/verification' element={<AccountVerification/>} />
