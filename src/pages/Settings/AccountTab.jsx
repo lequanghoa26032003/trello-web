acc8d9e07ef8d969d152172b7816ec03856828b1
lequanghoa26032003 <117667583+lequanghoa26032003@users.noreply.github.com>
2024-11-22 10:02:38 +0700

    lesson_11

--- src/pages/Settings/AccountTab.jsx
+++ src/pages/Settings/AccountTab.jsx
@@ -66,7 +66,7 @@ function AccountTab() {
 
   const uploadAvatar = (e) => {
     // Lấy file thông qua e.target?.files[0] và validate nó trước khi xử lý
-    console.log('e.target?.files[0]: ', e.target?.files[0])
+    // console.log('e.target?.files[0]: ', e.target?.files[0])
     const error = singleFileValidator(e.target?.files[0])
     if (error) {
       toast.error(error)
@@ -77,12 +77,23 @@ function AccountTab() {
     let reqData = new FormData()
     reqData.append('avatar', e.target?.files[0])
     // Cách để log được dữ liệu thông qua FormData
-    console.log('reqData: ', reqData)
-    for (const value of reqData.values()) {
-      console.log('reqData Value: ', value)
-    }
+    // console.log('reqData: ', reqData)
+    // for (const value of reqData.values()) {
+    //   console.log('reqData Value: ', value)
+    // }
 
     // Gọi API...
+    toast.promise(
+      dispatch(updateUserAPI(reqData)),
+      { pending: 'Updating...' }
+    ). then( res => {
+      // console.log(res)
+      //
+      if (!res.error) {
+        toast.success('Updated successfully!')
+      }
+      e.target.value = ''
+    })
   }
 
   return (
