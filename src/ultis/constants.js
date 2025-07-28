05ac93543ccd460e4c8ab221406947eefa9a1be1
lequanghoa26032003 <117667583+lequanghoa26032003@users.noreply.github.com>
2025-07-28 17:56:10 +0700

    fix

--- src/ultis/constants.js
+++ src/ultis/constants.js
@@ -1,4 +1,12 @@
-export const API_ROOT = 'http://localhost:8017'
+let apiRoot = ''
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://trello-api-rzmn.onrender.com'
+}
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8017'
+}
+
+export const API_ROOT = apiRoot
 export const DEFAULT_PAGE = 1
 export const DEFAULT_ITEMS_PER_PAGE = 12
 export const CARD_MEMBER_ACTIONS = {
